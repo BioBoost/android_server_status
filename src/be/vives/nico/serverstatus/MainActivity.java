749e5a4e75d74306769003d31ae6fa31c4628f99
Nico De Witte <nico.dewitte@khbo.be>
2014-11-15 15:39:16 +0100

    Add primitive status report display of the targets

--- src/be/vives/nico/serverstatus/MainActivity.java
+++ src/be/vives/nico/serverstatus/MainActivity.java
@@ -1,9 +1,13 @@
 package be.vives.nico.serverstatus;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
@@ -31,5 +35,60 @@ public class MainActivity extends Activity {
 		Toast.makeText(this, "Stopping Server Status Service", Toast.LENGTH_LONG).show();
 		ServerStatusService.stopScheduling(this);
 		Log.v(TAG, "Stopping Server Status Service");
+	}
+	
+	public void displayStats(View V)
+	{
+		TableLayout table = (TableLayout)findViewById(R.id.tblStatResults);
+		table.removeAllViews();
+		
+		//Create headers
+	    TableRow tableRow = new TableRow(this);
+
+	    TextView t = new TextView(this);
+	    t.setText("Target");
+
+	    TextView s = new TextView(this);
+	    s.setText("Success");
+	    s.setBackgroundColor(Color.GREEN);
+
+	    TextView f = new TextView(this);
+	    f.setText("Fails");
+	    f.setBackgroundColor(Color.MAGENTA);
+
+	    TextView sf = new TextView(this);
+	    sf.setText("Subseq fails");
+	    sf.setBackgroundColor(Color.RED);
+
+	    tableRow.addView(t);
+	    tableRow.addView(s);
+	    tableRow.addView(f);
+	    tableRow.addView(sf);
+	    table.addView(tableRow);
+	    
+    	for (Target target : ServerStatusService.targets) {
+    	    TableRow tableRow2 = new TableRow(this);
+
+    	    TextView txtTarget = new TextView(this);
+    	    txtTarget.setText(target.getUri());
+
+    	    TextView txtSuccess = new TextView(this);
+    	    txtSuccess.setText(target.getStats().getSuccesses() + "");
+    	    txtSuccess.setBackgroundColor(Color.GREEN);
+
+    	    TextView txtFails = new TextView(this);
+    	    txtFails.setText(target.getStats().getFails() + "");
+    	    txtFails.setBackgroundColor(Color.MAGENTA);
+
+    	    TextView txtSubsequentFails = new TextView(this);
+    	    txtSubsequentFails.setText(target.getStats().getSubsequentFails() + "");
+    	    txtSubsequentFails.setBackgroundColor(Color.RED);
+
+    	    tableRow2.addView(txtTarget);
+    	    tableRow2.addView(txtSuccess);
+    	    tableRow2.addView(txtFails);
+    	    tableRow2.addView(txtSubsequentFails);
+    	    table.addView(tableRow2);
+    	}
 	}
 }
