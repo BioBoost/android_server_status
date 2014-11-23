5e16a379bba152f60f4fa37314e3b96b47d9b655
Nico De Witte <nico.dewitte@khbo.be>
2014-11-23 17:40:43 +0100

    Remove the old status report from the main UI

--- src/be/vives/nico/serverstatus/MainActivity.java
+++ src/be/vives/nico/serverstatus/MainActivity.java
@@ -38,61 +38,6 @@ public class MainActivity extends Activity {
 		Log.v(TAG, "Stopping Server Status Service");
 	}
 	
-	public void displayStats(View V)
-	{
-		TableLayout table = (TableLayout)findViewById(R.id.tblStatResults);
-		table.removeAllViews();
-		
-		//Create headers
-	    TableRow tableRow = new TableRow(this);
-
-	    TextView t = new TextView(this);
-	    t.setText("Target");
-
-	    TextView s = new TextView(this);
-	    s.setText("Success");
-	    s.setBackgroundColor(Color.GREEN);
-
-	    TextView f = new TextView(this);
-	    f.setText("Fails");
-	    f.setBackgroundColor(Color.MAGENTA);
-
-	    TextView sf = new TextView(this);
-	    sf.setText("Subseq fails");
-	    sf.setBackgroundColor(Color.RED);
-
-	    tableRow.addView(t);
-	    tableRow.addView(s);
-	    tableRow.addView(f);
-	    tableRow.addView(sf);
-	    table.addView(tableRow);
-	    
-    	for (Target target : ServerStatusService.targets) {
-    	    TableRow tableRow2 = new TableRow(this);
-
-    	    TextView txtTarget = new TextView(this);
-    	    txtTarget.setText(target.getUri());
-
-    	    TextView txtSuccess = new TextView(this);
-    	    txtSuccess.setText(target.getStats().getSuccesses() + "");
-    	    txtSuccess.setBackgroundColor(Color.GREEN);
-
-    	    TextView txtFails = new TextView(this);
-    	    txtFails.setText(target.getStats().getFails() + "");
-    	    txtFails.setBackgroundColor(Color.MAGENTA);
-
-    	    TextView txtSubsequentFails = new TextView(this);
-    	    txtSubsequentFails.setText(target.getStats().getSubsequentFails() + "");
-    	    txtSubsequentFails.setBackgroundColor(Color.RED);
-
-    	    tableRow2.addView(txtTarget);
-    	    tableRow2.addView(txtSuccess);
-    	    tableRow2.addView(txtFails);
-    	    tableRow2.addView(txtSubsequentFails);
-    	    table.addView(tableRow2);
-    	}
-	}
-	
 	public void displayTargets(View v) {
 		startActivity(new Intent(this, TargetOverviewActivity.class));
 	}
